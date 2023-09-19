7784701b9803cc2effa375bc2dadf12efee3148c
Ayane <ayane@vignetteapp.org>
2023-09-19 14:34:04 +0800

    Make footer fixed

--- islands/Footer.tsx
+++ islands/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-300 shadow md:flex md:items-center md:justify-between md:p-6">
+    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-300 shadow md:flex md:items-center md:justify-between md:p-6">
       <span class="text-sm text-gray-500 sm:text-center">
         © 2023{" "}
         <a href="https://git.io/sr229" class="hover:underline">Ayase Minori</a>.
