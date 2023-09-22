6619815a57785757e23dc3038694a31c9eedd5e7
Ayase Minori <chinodesuuu@gmail.com>
2023-09-22 22:35:39 +0800

    Add Site analytics

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src='https://takeback.bysourfruit.com/api/kit/7NHKHAQY12LGE1WUP9Z806' />
         <title>Kuru kuru~!</title>
       </head>
       <body>
