54f336b4961ab1570c73357ac7ab8c6353460751
Ayane Satomi <chinodesuuu@gmail.com>
2023-10-21 08:54:24 +0000

    feat: Update meta tags and script source
    Update meta tags for Open Graph and description, and update script source URL.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,15 +5,33 @@ export default function App({ Component }: AppProps) {
   return (
     <html lang="en">
       <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="title" content="Herta Kuru"/>
-        <meta name="description" content="The website for Herta, the cutest genius Honkai: Star Rail character out there."/>
-        <meta name="keywords" content="Kuru2, Kuru Kuru, Kuru Kuru herta, kuru kuru herta hsr, kuru kuru herta star rail, herta honkai star rail, herta, herta hsr, star rail herta"/>
-        <meta name="robots" content="index, follow"/>
-        <meta name="language" content="English"/>
-        <meta name="canonical" content="https://herta.deno.dev/"/>
-        <script src='https://takeback.bysourfruit.com/api/kit/7NHKHAQY12LGE1WUP9Z806' />
+        <meta property="og:title" content="Kuru Kuru~" />
+        <meta property="og:site_name" content="Kuru Kuru~" />
+        <meta property="og:url" content="https://herta.deno.dev" />
+        <meta
+          property="og:description"
+          content="The website for Herta, the cutest genius Honkai: Star Rail character out there"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://herta.deno.dev/assets/img/hertaa1.gif"
+        />
+        <meta name="title" content="Herta Kuru" />
+        <meta
+          name="description"
+          content="The website for Herta, the cutest genius Honkai: Star Rail character out there."
+        />
+        <meta
+          name="keywords"
+          content="Kuru2, Kuru Kuru, Kuru Kuru herta, kuru kuru herta hsr, kuru kuru herta star rail, herta honkai star rail, herta, herta hsr, star rail herta"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <meta name="canonical" content="https://herta.deno.dev/" />
+        <script src="https://takeback.bysourfruit.com/api/kit/7NHKHAQY12LGE1WUP9Z806" />
         <title>Kuru kuru~!</title>
       </head>
       <body>
