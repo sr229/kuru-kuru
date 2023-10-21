f8716cdf26d08b82f737701527c0efcdfddf3833
Ayane Satomi <chinodesuuu@gmail.com>
2023-10-21 08:34:00 +0000

    Improve search engine visibility using values from herta.eu.org
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,9 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="The website for Herta, the cutest genius Honkai: Star Rail character out there."/>
+        <meta name="keywords" content="Kuru2, Kuru Kuru, Kuru Kuru herta, kuru kuru herta hsr, kuru kuru herta star rail, herta honkai star rail, herta, herta hsr, star rail herta"/>
+        <meta name="canonical" content="https://kuru-kuru.deno.dev"/>        
         <script src='https://takeback.bysourfruit.com/api/kit/7NHKHAQY12LGE1WUP9Z806' />
         <title>Kuru kuru~!</title>
       </head>
