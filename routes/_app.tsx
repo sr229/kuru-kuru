417c4d016e8750a06648c2b37eb1934f726477e0
Ayase Minori <chinodesuuu@gmail.com>
2023-10-21 16:45:32 +0800

    Yo dawg I heard you like SEO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,11 +5,14 @@ export default function App({ Component }: AppProps) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="title" content="Herta Kuru"/>
         <meta name="description" content="The website for Herta, the cutest genius Honkai: Star Rail character out there."/>
         <meta name="keywords" content="Kuru2, Kuru Kuru, Kuru Kuru herta, kuru kuru herta hsr, kuru kuru herta star rail, herta honkai star rail, herta, herta hsr, star rail herta"/>
-        <meta name="canonical" content="https://herta.deno.dev/"/>        
+        <meta name="robots" content="index, follow"/>
+        <meta name="language" content="English"/>
+        <meta name="canonical" content="https://herta.deno.dev/"/>
         <script src='https://takeback.bysourfruit.com/api/kit/7NHKHAQY12LGE1WUP9Z806' />
         <title>Kuru kuru~!</title>
       </head>
