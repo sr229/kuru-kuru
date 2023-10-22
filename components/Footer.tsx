ab29f592ebd8ebb92eabb87a42526d34ffb51a70
Ayane Satomi <chinodesuuu@gmail.com>
2023-10-22 06:00:43 +0000

    style: Updated Footer component
    Removed unnecessary class and added line breaks for readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-300 shadow md:flex md:items-center md:justify-between md:p-6">
+    <footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-300 md:flex md:items-center md:justify-between md:p-6">
       <span class="text-sm text-gray-500 sm:text-center">
         © 2023{" "}
         <a href="https://git.io/sr229" class="hover:underline">Ayase Minori</a>.
@@ -8,10 +8,20 @@ export default function Footer() {
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
         <li>
-          <a href="https://github.com/sr229/kuru-kuru" class="mr-4 hover:underline md:mr-6">GitHub Repo</a>
+          <a
+            href="https://github.com/sr229/kuru-kuru"
+            class="mr-4 hover:underline md:mr-6"
+          >
+            GitHub Repo
+          </a>
         </li>
         <li>
-          <a href="https://duiqt.github.io/herta_kuru/" class="mr-4 hover:underline md:mr-6">Visit the original!</a>
+          <a
+            href="https://duiqt.github.io/herta_kuru/"
+            class="mr-4 hover:underline md:mr-6"
+          >
+            Visit the original!
+          </a>
         </li>
       </ul>
     </footer>
