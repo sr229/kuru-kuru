33c03ca07838075b5ab16bb703943130c175b4c4
Ayane Satomi <chinodesuuu@gmail.com>
2023-10-22 06:38:31 +0000

    fix: Remove script tag
    Removed a script tag from _app.tsx file.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -36,7 +36,6 @@ export default function App({ Component }: AppProps) {
       </head>
       <body>
         <Component />
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "da71f3cc23af449aa491996d8d2d1eb5"}'></script>
       </body>
       <Footer />
     </html>
