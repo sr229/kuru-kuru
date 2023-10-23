ee4f5b05e00549b012a54755aba849a7bbcaddfb
Ayane Satomi <chinodesuuu@gmail.com>
2023-10-23 16:09:50 +0000

    Revert db to use numbers again

--- shared/db.ts
+++ shared/db.ts
@@ -1,12 +1,11 @@
 const kv = await Deno.openKv();
 
 export async function getGlobalStatistics() {
-  const res = await kv.get<bigint>(["global-statistics"])!;
+  const res = await kv.get<number>(["global-statistics"])!;
   return res.value!;
 }
 
 export async function setGlobalStatistics(value: number) {
   const pv = await getGlobalStatistics();
-  const converted = BigInt(value);
-  await kv.set(["global-statistics"], pv + converted);
+  await kv.set(["global-statistics"], pv + value);
 }
