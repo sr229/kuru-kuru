1d934ca4daa3ccef6899952d43287dc4d9a4b4e5
Ayase Minori <chinodesuuu@gmail.com>
2025-01-12 16:36:37 +0000

    Ensure year is always updated so I dont have to change this pesky thing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
+  const year = new Date().getFullYear().toString();
+
   return (
     <footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-300 md:flex md:items-center md:justify-between md:p-6">
       <span class="text-sm text-gray-500 sm:text-center">
-        © 2023{" "}
+        &copy; {year}{" "}
         <a href="https://git.io/sr229" class="hover:underline">Ayase Minori</a>.
         All Rights Reserved. Licensed under MIT.
       </span>
